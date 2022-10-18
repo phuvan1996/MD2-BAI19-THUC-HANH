c0a9d3aeda32b73b2b484eff0bcfc53172a85a7f
phuvan1996 <phuvan2604@gmail.com>
2022-10-18 23:30:02 +0700

    MD2-BAI 19-THUC HANH

---
+++ src/thuchanh2/AccountExampleTest.java
@@ -0,0 +1,18 @@
+package thuchanh2;
+
+public class AccountExampleTest {
+    private static AccountExample accountExample;
+    public static final String[] validAccount = new String[]{"123abc_","_abc123","_____","123456","abcefgh"};
+    public static final String[] invalidAccount = new String[]{".@","12345","1234_","abcde"};
+    public static void main(String args[]){
+        accountExample = new AccountExample();
+        for (String account:validAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is "+account+"is valid: "+isvalid);
+        }
+        for (String account:invalidAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is "+account+"is valid: "+isvalid);
+        }
+    }
+}
